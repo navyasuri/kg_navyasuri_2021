3712e8fe77961c9238bb77fc24d271abae7f11a6
Nav <navyasuri@nyu.edu>
2020-03-20 21:40:19 +0400

    Cleaned up, optimized function, commented and removed debugging

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import argparse
 from collections import Counter
 
+'''
+NOTE (s): 
+The following code is an O(nlogn) solution. 
+ASSUMPTIONS: 
+1. Unequal strings always return false
+2. The order of characters in the strings does not matter. 
+   For example, s1=aab, s2=dcc will print "true"
+   
+'''
+
 def check_mapping(s1, s2):
 
+    # Unequal lengths will return false
     if (len(s1) != len(s2)): 
         return False
 
@@ -12,21 +23,17 @@ def check_mapping(s1, s2):
 
     l1, l2 = len(c1.values()), len(c2.values())
 
-    print(c1, c2)
-
-    # Make sure s1 has more unique chars than s2
+    # Make sure s1 has equal or more unique chars than s2
     if l1 < l2:
-        print('from here')
         return False
 
-    
-    for a, b in zip(sorted(c1.values(), reverse=True), sorted(c2.values(), reverse=True)):
+    for (_, a), (_, b) in zip(c1.most_common(), c2.most_common()):
         if a < b and l1 <= l2:
-            print(a, b)
             return False
+
     return True
 
-
+# Parsing arguments
 parser = argparse.ArgumentParser(description="Discover one-to-one mapping between 2 strings")
 parser.add_argument('s1', type=str, help="Enter s1")
 parser.add_argument('s2', type=str, help="Enter s2")
@@ -34,4 +41,4 @@ parser.add_argument('s2', type=str, help="Enter s2")
 args = parser.parse_args()
 s1, s2 = args.s1, args.s2
 
-print(check_mapping(s1, s2))
+print(str(check_mapping(s1, s2)).lower())
